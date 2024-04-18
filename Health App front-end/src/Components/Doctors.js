cf7821d22a34081c0b798c62b9dd27580d9dc291
Shubham Sharma <sharmashibu787@gmail.com>
2024-04-18 19:09:31 +0530

    Update card Doctors.js

--- Health App front-end/src/Components/Doctors.js
+++ Health App front-end/src/Components/Doctors.js
@@ -5,7 +5,7 @@ import profile2 from "../Assets/profile-2.png";
 import profile3 from "../Assets/profile-3.png";
 import profile4 from "../Assets/profile-4.png";
 import "../Styles/Doctors.css";
-
+ 
 function Doctors() {
   return (
     <div className="doctor-section" id="doctors">
